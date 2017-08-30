6bb982f8a221fd134614200eadde643c6a7bbd11
Younggil Lee <leyoungg@sheridancollege.ca>
2017-08-30 16:56:31 -0400

    Implementing searching bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React,{Component} from 'react';
 
-const SearchBar = () => {
+class SearchBar extends Component {
+    constructor(props){
+        super(props);
 
-    return <input />;
-};
+        this.state = {term: ''};
+    }
+
+    render() {
+       // return <input onChange={(event) => console.log(event.target.value)} />;
+
+       return (
+           <div>
+                <input onChange={(event) => this.setState({term: event.target.value})} />
+                {/* Value of the input: {this.state.term} */}
+          </div>
+       );
+    
+    }
+
+   
+
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY='AIzaSyA7V09MtA4XmnhyrPqoyEtHUbPw3l_Dxco';
 
 const App = () => {
-    return <div>Test</div>;
+    return (<div>
+        <SearchBar />
+    </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));

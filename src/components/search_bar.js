df2ed783cd328155faeff5eb05b62f791b8bc874
Younggil Lee <leyoungg@sheridancollege.ca>
2017-08-31 15:57:50 -0400

    implement video list and list times

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,7 +12,9 @@ class SearchBar extends Component {
 
        return (
            <div>
-                <input onChange={(event) => this.setState({term: event.target.value})} />
+                <input
+                 value={this.state.term}
+                 onChange={(event) => this.setState({term: event.target.value})} />
                 {/* Value of the input: {this.state.term} */}
           </div>
        );

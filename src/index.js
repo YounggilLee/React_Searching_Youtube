ec5e3a2df10d765b3ddee2229b4ccd4a0ddf94f7
Younggil Lee <leyoungg@sheridancollege.ca>
2017-09-01 10:25:52 -0400

    implement video list item

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY='AIzaSyA7V09MtA4XmnhyrPqoyEtHUbPw3l_Dxco';
 
@@ -21,6 +22,7 @@ constructor(props) {
         return (
         <div>
             <SearchBar />
+            <VideoDetail video={this.state.videos[0]}/>
             <VideoList videos={this.state.videos} />
         </div>
         );

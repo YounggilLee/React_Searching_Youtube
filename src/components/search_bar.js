6df16637cc38128fd4a89b4070be5fc5be710327
Younggil Lee <leyoungg@sheridancollege.ca>
2017-09-05 09:17:01 -0400

    implement video detail

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,15 +12,19 @@ class SearchBar extends Component {
 
        return (
            <div>
-                <input
+                <input className='search-bar'
                  value={this.state.term}
-                 onChange={(event) => this.setState({term: event.target.value})} />
+                 onChange={(event) => this.onInputChange(event.target.value)} />
                 {/* Value of the input: {this.state.term} */}
           </div>
        );
     
     }
 
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
+      }
    
 
 }
--- src/index.js
+++ src/index.js
@@ -8,22 +8,34 @@ import VideoDetail from './components/video_detail';
 const API_KEY='AIzaSyA7V09MtA4XmnhyrPqoyEtHUbPw3l_Dxco';
 
 class App extends Component {
+
 constructor(props) {
     super(props);
-    this.state = { videos : []};
+    this.state = { 
+        videos : [],
+        selectedVideo : null
+        };
 
-    YTSearch({key:API_KEY, term:'snowboards'}, (videos) => {
-        //console.log(data);
-        this.setState({videos}); // this.setState({videos: videos});
-    });
-}
+        this.videoSearch('sonwboards');     
+    }
+    videoSearch(term) {
+        YTSearch(
+            {key:API_KEY, term:term}, (data) => {
+            this.setState({
+            videos: data,
+            selectedVideo: data[0]
+          });
+        });  
+      }
 
     render(){
         return (
         <div>
-            <SearchBar />
-            <VideoDetail video={this.state.videos[0]}/>
-            <VideoList videos={this.state.videos} />
+            <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+            <VideoDetail video={this.state.selectedVideo}/>
+            <VideoList 
+            onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
+            videos={this.state.videos} />
         </div>
         );
     }
